02d0ec81ce0019c7d4b8577b90eb5b116a82d818
rohan <rohan@ownedoutcomes.com>
2019-08-07 14:50:18 -0700

    First Commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,21 @@
+
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html',
+                  {'posts' : posts})
+
+def post_detail(request, year, month, day, posts):
+    posts = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    return render(request, 'blog/post/details.html',
+                  {'posts' : posts})
